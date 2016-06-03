49dc810059eb1a57cbb025cbcb6f2b37e378cc83
rh <ryanjharvey88@gmail.com>
2016-06-03 08:41:30 -0700

    Set up project skeleton

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {Food} from './food.model';
+
+@Component({
+  selector: 'my-app',
+  template:`
+    <h1>HI</h1>
+  `
+})
+
+export class AppComponent{}
