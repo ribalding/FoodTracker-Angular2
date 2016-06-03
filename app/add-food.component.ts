87024324ced1babe413076dff272e005418ca6bb
rh <ryanjharvey88@gmail.com>
2016-06-03 09:14:10 -0700

    Added add-food component and functionality to emit the new food object to parent component to use in food list

---
+++ app/add-food.component.ts
@@ -0,0 +1,26 @@
+import {Food} from './food.model';
+import {Component, EventEmitter} from 'angular2/core';
+
+@Component({
+  selector: 'add-food',
+  outputs: ['onSubmit'],
+  template: `
+    <h3>Add A New Food:</h3>
+    <input type="text" placeholder="Name" class="input-lg" #inputName>
+    <input type="text" placeholder="Details" class="input-lg" #inputDetails>
+    <input type="text" placeholder="Calories" class="input-lg" #inputCalories>
+    <button (click)='addFood(inputName, inputDetails, inputCalories)'>Submit</button>
+  `
+})
+
+export class AddFoodComponent{
+  public onSubmit: EventEmitter<Food>;
+  constructor(){
+    this.onSubmit = new EventEmitter();
+  }
+  addFood(name: HTMLInputElement, details: HTMLInputElement, calories: HTMLInputElement){
+    var newFood = new Food(name.value, details.value, parseInt(calories.value));
+    console.log(newFood);
+    this.onSubmit.emit(newFood);
+  }
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,23 @@
 import {Component} from 'angular2/core';
 import {Food} from './food.model';
+import {AddFoodComponent} from './add-food.component';
 
 @Component({
   selector: 'my-app',
+  directives: [AddFoodComponent],
   template:`
-    <h1>HI</h1>
+    <div class="container">
+      <h1>YOUR FOOD BRO</h1>
+      <add-food (onSubmit)='addToFoodList($event)'></add-food>
+    </div>
   `
 })
 
-export class AppComponent{}
+export class AppComponent{
+  public foodList = [];
+  constructor(){}
+  addToFoodList(food: Food){
+    this.foodList.push(food);
+    console.log(this.foodList);
+  }
+}
